6c75580dafa838d6657123f9777f8f2fc1b17f75
Leonard <trufelnaisveni@gmail.com>
2021-11-09 19:35:33 +0300

    Add Project1

---
+++ Project1/Project1/Project1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    float x1,x2,x3,y1,y2,y3,m1,m2,m3;
+    cout << "Vvedite coordinati pervoi materialnoi tochki i massy(x, y, m)" << endl;
+    cin >> x1 >> y1 >> m1;
+    cout << "Vvedite coordinati vtoroi materialnoi tochki i massy(x, y, m)" << endl;
+    cin >> x2 >> y2 >> m2;
+    cout << "Vvedite coordinati treti materialnoi tochki i massy(x, y, m)" << endl;
+    cin >> x3 >> y3 >> m3;
+    cout << "Xc = " << (x1 * m1 + x2 * m2 + x3 * m3) / (m1 + m2 + m3) << " Yc = " << (y1 * m1 + y2 * m2 + y3 * m3) / (m1 + m2 + m3);
+}
