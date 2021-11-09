2746c45a5929524252ad0bfe71bf2bfdbab927cf
Leonard <trufelnaisveni@gmail.com>
2021-11-10 00:13:01 +0300

    Project 10 complete

---
+++ Project10/Project10/Project10.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string temp;
+    ifstream inf;
+    inf.open("f.txt");
+    if (inf.is_open()) {
+        getline(inf, temp);
+    }
+    inf.close();
+    for (int i = 0; i < temp.length(); i++) {
+        if (temp[i] == '9') {
+            temp[i] = '1';
+        } else if (temp[i] == '1') {
+            temp[i] = '9';
+        }
+    }
+    remove("f.txt");
+    ofstream inf1("f.txt");
+    inf1 << temp;
+    inf1.close();
+}
