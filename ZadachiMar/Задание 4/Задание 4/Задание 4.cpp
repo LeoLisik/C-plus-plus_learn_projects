7a24c85ee71e1b25ff4d891886f9a871349ee9a3
SHIM <mapat717@gmail.com>
2021-11-22 15:27:50 +0300

    NewFactor

---
+++ ZadachiMar/Задание 4/Задание 4/Задание 4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	double E, sum = 0, s, f;
+	do
+	{
+		cout << "Vvedite n " << endl;
+		cin >> n;
+	} while (n < 1);
+	for (float k = 1; k <= n; k++)
+	{
+		f = (pow((-1) , k)/ ((2*k + 1 )*k));
+		sum = sum + f;
+	}
+	cout << "Summa pervih n chlenov = " << sum << endl;
+	do
+	{
+		cout << "Vvedite E" << endl;
+		cin >> E;
+	} while (E >= 1);
+	for (double k = 1; k++;)
+	{
+		s = (pow((-1), k) / ((2 * k + 1) * k));
+		if (abs(s) >= E)
+		{
+			cout << "А" << k << "= " << s << endl;
+		}
+
+	}
+
+}
---
+++ ZadachiMar/Задание 5.2/Задание 5.2/Задание 5.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    const int n = 8;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Введите текст: ";
+    string s;
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'А') { s[i] = 'Е'; }
+        if (s[i] == 'Б') { s[i] = 'Ж'; }
+        if (s[i] == 'В') { s[i] = 'Ч'; }
+        if (s[i] == 'Г') { s[i] = 'Б'; }
+    }
+    cout << s << endl;
+    system("pause");
+    return 0;
+
+}
