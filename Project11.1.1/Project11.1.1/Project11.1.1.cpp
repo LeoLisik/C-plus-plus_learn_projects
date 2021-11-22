8320a7e911dbe6966a07a6364e2568345abaf462
Leonard <trufelnaisveni@gmail.com>
2021-11-22 21:41:25 +0300

    last 7,9 and 13

--- Project11.1.1/Project11.1.1/Project11.1.1.cpp
+++ Project11.1.1/Project11.1.1/Project11.1.1.cpp
@@ -1,73 +1,61 @@
 ﻿#include <iostream>
-#include <conio.h>
-#include <math.h>
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+#define ch s.find_last_not_of("-0123456789.")
+#define ch2 s.find_last_of("-")
+#define ch3  s.find_last_of(".")-s.find_first_of(".")
 
-using std::cout;
-using std::endl;
-using std::cin;
-
-// функция, проверяющая, простое число или нет
-int primeNumber(int number)
-{
-    for (int i = 2; i < number; i++)
-    {
-        if (number % i == 0)
-            return 0;
-        if ((i == number)  (i > sqrt((double)number)))
-            return 1;
-    }
-    return 1;
-}
-
-int main()
-{
-    int n;
-    cout << "Vvedite chislo  n: ";
-    cin >> n;
-
-    // подсчитываем кол-во элементов для массива простых чисел, не включающего 2, 3 и 5
-    int count = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        if ((primeNumber(i) == 1) && (i != 2) && (i != 3) && (i != 5))
-            count++;
-    }
-
-    // выделяем для него память
-    int* arr = new int[count];
-
-    // заполняем его
-    for (int i = 2, j = 0; i <= n; i++)
-    {
-        if (primeNumber(i) == 1)
-        {
-            if ((i != 2) && (i != 3) && (i != 5))
-            {
-                arr[j] = i;
-                j++;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    string s;
+    int* result = new int;
+    int n, m, naideno = 0, a = 1, elements = 0, temp;
+    do {
+        do {
+            cout << "Введите натуральное число = ";
+            getline(cin, s);
+        } while ((ch != -1) or (ch2 != -1 and ch2 != 0) or (ch3 != 0));
+        n = atof(s.c_str());
+    } while (n < 1);
+    do {
+        do {
+            cout << "Введите количество чисел = ";
+            getline(cin, s);
+        } while ((ch != -1) or (ch2 != -1 and ch2 != 0) or (ch3 != 0));
+        m = atof(s.c_str());
+    } while (m < 1);
+    while (naideno < m) {
+        n++;
+        cout << "n = " << n << endl;
+        temp = n;
+        while (a != 0) {
+            if (temp % 5 == 0) {
+                temp /= 5;
             }
+            else if (temp % 3 == 0) {
+                temp /= 3;
+            }
+            else if (temp % 2 == 0) {
+                temp /= 2;
+            }
+            else if (temp == 1) {
+                naideno++;
+                result[elements] = n;
+                elements++;
+                break;
+            }
+            else {
+                break;
+            }
+            cout << "Всё работает. temp = " << temp << endl;
         }
     }
-
-    // получаем в порядке возрастания n первых натуральных чисел,
-    // которые не делятся ни на какие простые числа, кроме 2, 3 и 5
-    cout << "Result: " << endl;
-    for (int i = 1; i < n; i++)
-    {
-        if ((i % 2 == 0)  (i % 3 == 0) || (i % 5 == 0))
-        {
-            for (int j = 0; j < count; j++)
-            {
-                if (i % arr[j] != 0)
-                {
-                    cout << i << ' ';
-                    break;
-                }
-            }
-        }
+    cout << "Результат = ";
+    for (int i = 0; i < elements; i++) {
+        cout << result[i] << ' ';
     }
-
-    delete[] arr;
-    _getch();
-    return 0;
+    delete[]result;
 }
