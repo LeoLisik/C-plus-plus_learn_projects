621a9306e684af5b84c083a29bd2705efceeb149
Leonard <trufelnaisveni@gmail.com>
2021-11-24 00:17:37 +0300

    Update Project7

--- Project7/Project7/Project7.cpp
+++ Project7/Project7/Project7.cpp
@@ -1,21 +1,31 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <string>
 using namespace std;
 
-float hight(float V, float t) {
-    return (V * t) / 2;
+float Vvod_chislo(string s, string b = "", float n = 0) {
+    do {
+        do {
+            cout << s << " = ";
+            getline(cin, b);
+        } while ((b.find_last_not_of("-0123456789.") != -1) or (b.find_last_of("-") != -1 and b.find_last_of("-") != 0) or (b.find_last_of(".") - b.find_first_of(".") != 0));
+        n = atof(b.c_str());
+    } while (n < 0.1);
+    return n;
 }
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    float N, V, t, h, const g = 9.8, v2;
-    cout << "Введите высоту(м) = ";
-    cin >> N;
-    cout << "Введите начальную скорость(м/c) = ";
-    cin >> V;
-    cout << "Введите время подъёма тела на максимальную высоту(сек) = ";
-    cin >> t;
-    h = hight(V, t);
-    v2 = 2 * g * h;
+    float N, V, t;
+    float const g = 9.8;
+    float* Height = new float[];
+    float* Speed = new float[];
+    float* Time = new float[];
+    N = Vvod_chislo("Введите высоту(м)");
+    V = Vvod_chislo("Введите начальную скорость(м/c)");
+    t = Vvod_chislo("Введите время(сек)");
+    for (int i = 0, el = 0; i < t; i += 0.25, el++) {
+
+    }
 }
