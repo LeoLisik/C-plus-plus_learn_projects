dba847d35cb12735b1fc033c18cc8a996081f68d
Leonard <trufelnaisveni@gmail.com>
2022-01-30 13:00:47 +0300

    Update Project 2

--- Second part of learn/Project 2/Project 2/Project 2.cpp
+++ Second part of learn/Project 2/Project 2/Project 2.cpp
@@ -1,7 +1,97 @@
 ﻿#include <iostream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
 using namespace std;
 
-int main()
+class Point;
+class Triangle;
+class Line;
+
+class Point
 {
-    
+	friend Triangle;
+	friend Line;
+private:
+	int x, y;
+	char ch;
+public:
+	Point(int x, int y, char ch);
+	static void gotoxy(int xpos, int ypos);
+};
+
+void Point::gotoxy(int xpos, int ypos)
+{
+	COORD position;
+	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
+	position.X = xpos;
+	position.Y = ypos;
+	SetConsoleCursorPosition(screen, position);
+}
+
+Point::Point(int x, int y, char ch)
+{
+	this->x = x;
+	this->y = y;
+	this->ch = ch;
+}
+
+class Triangle
+{
+public:
+	Triangle(Point& a, Point& b, Point& c);
+};
+
+Triangle::Triangle(Point& a, Point& b, Point& c)
+{
+	Point::gotoxy(a.x, a.y);
+	_putch(a.ch);
+
+	Point::gotoxy(b.x, b.y);
+	_putch(b.ch);
+
+	Point::gotoxy(c.x, c.y);
+	_putch(c.ch);
+}
+
+class Line
+{
+public:
+	Line(Point& a, Point& b);
+};
+
+Line::Line(Point& a, Point& b)
+{
+	Point::gotoxy(a.x, a.y);
+	_putch(a.ch);
+
+	Point::gotoxy(b.x, b.y);
+	_putch(b.ch);
+}
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Point a(5, 5, '·');
+	Point b(10, 5, '·');
+	Point c(5, 10, '·');
+
+	Triangle abc(a, b, c);
+	cout << endl << endl << endl << endl;
+	system("pause");
+	system("cls");
+
+	Line ab(a, b);
+	cout << endl << endl << endl << endl;
+	system("pause");
+	system("cls");
+
+	Line ac(a, c);
+	cout << endl << endl << endl << endl;
+	system("pause");
+	system("cls");
+
+	Line bc(b, c);
+	cout << endl << endl << endl << endl;
 }
