aa332f2b33df4cf613ae586d4b453f63b06eb722
Leonard <trufelnaisveni@gmail.com>
2022-02-01 20:10:46 +0300

    Project 2 complete

--- Second part of learn/Project 2/Project 2/Project 2.cpp
+++ Second part of learn/Project 2/Project 2/Project 2.cpp
@@ -1,97 +1,71 @@
 ﻿#include <iostream>
-#include <string>
-#include <conio.h>
 #include <Windows.h>
 using namespace std;
 
-class Point;
-class Triangle;
-class Line;
-
 class Point
 {
-	friend Triangle;
-	friend Line;
+	friend class Line;
+	friend class Triangel;
+public:
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	static void gotoxy(int xpos, int ypos)
+	{
+		COORD position;
+		HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
+		position.X = xpos;
+		position.Y = ypos;
+		SetConsoleCursorPosition(screen, position);
+	}
+	~Point()
+	{
+		cout << "game";
+	}
 private:
 	int x, y;
-	char ch;
-public:
-	Point(int x, int y, char ch);
-	static void gotoxy(int xpos, int ypos);
 };
-
-void Point::gotoxy(int xpos, int ypos)
-{
-	COORD position;
-	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
-	position.X = xpos;
-	position.Y = ypos;
-	SetConsoleCursorPosition(screen, position);
-}
-
-Point::Point(int x, int y, char ch)
-{
-	this->x = x;
-	this->y = y;
-	this->ch = ch;
-}
-
-class Triangle
-{
-public:
-	Triangle(Point& a, Point& b, Point& c);
-};
-
-Triangle::Triangle(Point& a, Point& b, Point& c)
-{
-	Point::gotoxy(a.x, a.y);
-	_putch(a.ch);
-
-	Point::gotoxy(b.x, b.y);
-	_putch(b.ch);
-
-	Point::gotoxy(c.x, c.y);
-	_putch(c.ch);
-}
 
 class Line
 {
 public:
-	Line(Point& a, Point& b);
+	Line(Point& a, Point& b) {
+		Point::gotoxy(a.x, a.y);
+		putchar('.');
+		Point::gotoxy(b.x, b.y);
+		putchar('.');
+	}
 };
 
-Line::Line(Point& a, Point& b)
+class Triangel
 {
-	Point::gotoxy(a.x, a.y);
-	_putch(a.ch);
+public:
+	Triangel(Point& a, Point& b, Point& c) {
+		Point::gotoxy(a.x, a.y);
+		putchar('.');
+		Point::gotoxy(b.x, b.y);
+		putchar('.');
+		Point::gotoxy(c.x, c.y);
+		putchar('.');
+	};
+};
 
-	Point::gotoxy(b.x, b.y);
-	_putch(b.ch);
-}
-
-void main()
+int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	Point a(5, 5, '·');
-	Point b(10, 5, '·');
-	Point c(5, 10, '·');
-
-	Triangle abc(a, b, c);
-	cout << endl << endl << endl << endl;
-	system("pause");
+	Point A(40, 8);
+	Point B(50, 2);
+	Point C(20, 6);
+	Triangel(A, B, C);
+	Sleep(2000);
 	system("cls");
-
-	Line ab(a, b);
-	cout << endl << endl << endl << endl;
-	system("pause");
+	Line(A, B);
+	Sleep(2000);
 	system("cls");
-
-	Line ac(a, c);
-	cout << endl << endl << endl << endl;
-	system("pause");
+	Line(A, C);
+	Sleep(2000);
 	system("cls");
-
-	Line bc(b, c);
-	cout << endl << endl << endl << endl;
+	Line(B, C);
+	Sleep(2000);
+	system("cls");
 }
